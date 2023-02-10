a46f1daf3e0bf477af65f3fe291d35b3fe337d5b
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-10 15:41:41 +0700

    init project

---
+++ iOS16/iOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS16App.swift
+//  iOS16
+//
+//  Created by TheGIZzz on 10/2/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct iOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
