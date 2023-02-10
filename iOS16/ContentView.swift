c53648437bec35d690379adbc8cb2f79795ee7d9
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-10 21:15:43 +0700

    add layout and frame to modal

--- iOS16/ContentView.swift
+++ iOS16/ContentView.swift
@@ -10,28 +10,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(alignment: .center, spacing: 20.0) {
-            Image(systemName: "timelapse")
+            Image(systemName: "timelapse", variableValue: 0.2)
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                .font(.system(size: 50))
+                .fontWeight(.bold)
             Text("Switching Apps")
                 .font(.largeTitle)
                 .fontWeight(.bold)
             Text("Tap and hold any part of the screen for 1 second to show the menu for switching between apps.")
                 .multilineTextAlignment(.center)
-            Button("Got it") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            Button {
+                
+            } label: {
+                Text("Got it")
+                    .padding(.all)
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke()
+                    )
             }
-            .padding(.all)
-            .frame(width: 300.0)
-            .border(/*@START_MENU_TOKEN@*/Color("AccentColor")/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+            .accentColor(.primary)
         }
-        .padding()
+        .padding(30)
+        .background(.ultraThinMaterial)
+        .cornerRadius(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke()
+        )
+        .padding(10)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .background(Image("Wallpaper 2"))
     }
 }
