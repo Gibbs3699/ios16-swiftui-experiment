1e08cd71d637d8c32c6d1808570640fe19698547
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-10 20:59:09 +0700

    adding Switching Apps modal

--- iOS16/ContentView.swift
+++ iOS16/ContentView.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        VStack(alignment: .center, spacing: 20.0) {
+            Image(systemName: "timelapse")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+            Text("Switching Apps")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("Tap and hold any part of the screen for 1 second to show the menu for switching between apps.")
+                .multilineTextAlignment(.center)
+            Button("Got it") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .padding(.all)
+            .frame(width: 300.0)
+            .border(/*@START_MENU_TOKEN@*/Color("AccentColor")/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
         }
         .padding()
     }
