22d7c1ca9407a28ca54af5fdca50504957e4c0d9
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-14 16:45:56 +0700

    add width style to modal

--- iOS16/ContentView.swift
+++ iOS16/ContentView.swift
@@ -15,23 +15,28 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
                 .font(.system(size: 50))
                 .fontWeight(.bold)
-            Text("Switching Apps")
-                .font(.largeTitle)
+            Text("Switching Apps".uppercased())
+                .font(.largeTitle.width(.condensed))
                 .fontWeight(.bold)
             Text("Tap and hold any part of the screen for 1 second to show the menu for switching between apps.")
                 .multilineTextAlignment(.center)
+                .foregroundColor(.secondary)
             Button {
                 
             } label: {
                 Text("Got it")
                     .padding(.all)
                     .frame(maxWidth: .infinity)
+                    .background(.white.opacity(0.2).gradient)
+                    .cornerRadius(10)
                     .background(
                         RoundedRectangle(cornerRadius: 10)
                             .stroke()
+                            .foregroundStyle(.linearGradient(colors: [.white.opacity(0.5), .clear, .white.opacity(0.5), .clear], startPoint: .topLeading, endPoint: .bottomTrailing))
                     )
             }
             .accentColor(.primary)
+            .shadow(radius: 10)
         }
         .padding(30)
         .background(.ultraThinMaterial)
@@ -39,8 +44,12 @@ struct ContentView: View {
         .overlay(
             RoundedRectangle(cornerRadius: 10)
                 .stroke()
+                .foregroundStyle(.linearGradient(colors: [.white.opacity(0.5), .clear, .white.opacity(0.5), .clear], startPoint: .topLeading, endPoint: .bottomTrailing))
         )
+        .shadow(color: .black.opacity(0.3), radius: 20, y: 20)
+        .frame(maxWidth: 500)
         .padding(10)
+        .dynamicTypeSize(.xSmall ... .xxxLarge)
     }
 }
 
