944aa5bc656d1ae8b749e9de17c324942c4eba1f
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-15 14:39:48 +0700

    add data model

---
+++ iOS16/Data.swift
@@ -0,0 +1,35 @@
+//
+//  Data.swift
+//  iOS16
+//
+//  Created by TheGIZzz on 15/2/2566 BE.
+//
+
+import Foundation
+
+struct NavigationItem: Identifiable {
+    var id = UUID()
+    var title: String
+    var icon: String
+    var menu: Menu
+}
+
+var navigationItems = [
+    NavigationItem(title: "Compass App", icon: "safari", menu: .compass),
+    NavigationItem(title: "3D Card", icon: "lanyardcard", menu: .card),
+    NavigationItem(title: "Radial Layout", icon: "clock", menu: .radial),
+    NavigationItem(title: "Gooey Action Button", icon: "plus.circle", menu: .actionbutton),
+    NavigationItem(title: "Gooey Menu", icon: "drop", menu: .gooey),
+    NavigationItem(title: "Charts", icon: "chart.xyaxis.line", menu: .charts),
+    NavigationItem(title: "Half Sheet", icon: "rectangle.portrait.bottomhalf.filled", menu: .halfsheet),
+]
+
+enum Menu: String {
+    case compass
+    case card
+    case charts
+    case radial
+    case halfsheet
+    case gooey
+    case actionbutton
+}
