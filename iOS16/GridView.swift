9d0a2896c75e000e44b47cc0164f48f2c07cdc74
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-17 20:51:24 +0700

    add GridView

---
+++ iOS16/GridView.swift
@@ -0,0 +1,73 @@
+//
+//  GridView.swift
+//  iOS16
+//
+//  Created by TheGIZzz on 17/2/2566 BE.
+//
+
+import SwiftUI
+
+struct GridView: View {
+    var body: some View {
+        Grid(alignment: .leading, horizontalSpacing: 12, verticalSpacing: 12) {
+            GridRow {
+                Text("Votes")
+                Text("Percentage")
+                Text("Rating")
+            }
+            .font(.footnote)
+            .foregroundColor(.secondary)
+            Divider().gridCellUnsizedAxes(.horizontal)
+            GridRow {
+                Text("4")
+                    .frame(width: 30, alignment: .trailing)
+                ProgressView(value: 0.1)
+                RatingView(rating: 1)
+            }
+            GridRow {
+                Text("21")
+                    .frame(width: 30, alignment: .trailing)
+                ProgressView(value: 0.2)
+                RatingView(rating: 2)
+            }
+            GridRow {
+                Text("5")
+                    .frame(width: 30, alignment: .trailing)
+                ProgressView(value: 0.1)
+                RatingView(rating: 3)
+            }
+            GridRow {
+                Text("52")
+                    .frame(width: 30, alignment: .trailing)
+                ProgressView(value: 0.5)
+                RatingView(rating: 4)
+            }
+            GridRow {
+                Text("67")
+                    .frame(width: 30, alignment: .trailing)
+                ProgressView(value: 0.6)
+                RatingView(rating: 5)
+            }
+        }
+        .padding(20)
+    }
+}
+
+struct GridView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridView()
+    }
+}
+
+struct RatingView: View {
+    var rating = 3
+    let list = [1, 2, 3, 4, 5]
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(list, id: \.self) { item in
+                Image(systemName: rating >= item ? "star.fill" : "star")
+            }
+        }
+    }
+}
