52c4e7d26e0014042dbf170a78b7268cf9779f2a
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-20 16:00:28 +0700

    fix custom layout

--- iOS16/RadialLayoutView.swift
+++ iOS16/RadialLayoutView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct RadialLayoutView: View {
     var icons = ["calendar", "message", "figure.walk", "music.note"]
     var numbers = [12,1,2,3,4,5,6,7,8,9,10,11]
+    @State var isRadial = true
     
     var body: some View {
+        let layout = isRadial ? AnyLayout(RadialLayout()) : AnyLayout(CustomLayout())
+        
         ZStack {
-            RadialLayout {
+            layout {
                 ForEach(icons, id: \.self) { item in
                     Circle()
                         .frame(width: 44)
@@ -23,7 +26,7 @@ struct RadialLayoutView: View {
             }
             .frame(width: 120)
             
-            RadialLayout {
+            layout {
                 ForEach(numbers, id: \.self) { item in
                     Text("\(item)")
                         .font(.system(.title, design: .rounded))
@@ -33,7 +36,7 @@ struct RadialLayoutView: View {
             }
             .frame(width: 240)
             
-            RadialLayout {
+            layout {
                 ForEach(numbers, id: \.self) { item in
                     Text("\(item*5)")
                         .font(.system(.caption, design: .rounded))
@@ -46,6 +49,11 @@ struct RadialLayoutView: View {
             Circle()
                 .strokeBorder(.black, style: StrokeStyle(lineWidth: 10, dash: [1, 10]))
                 .frame(width: 220)
+        }
+        .onTapGesture {
+            withAnimation(.spring()) {
+                isRadial.toggle()
+            }
         }
     }
 }
@@ -75,4 +83,25 @@ struct RadialLayout: Layout {
         }
     }
     
+}
+
+struct CustomLayout: Layout {
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        proposal.replacingUnspecifiedDimensions()
+    }
+
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        for (index, subview) in subviews.enumerated() {
+            var point = CGPoint(x: 50 * index, y: 50 * index).applying(CGAffineTransform(rotationAngle: 5))
+
+            // Center
+            point.x += bounds.midX
+            point.y += bounds.midY
+
+            // Place subviews
+            subview.place(at: point, proposal: .unspecified)
+            
+            subview.place(at: point, anchor: .center, proposal: .unspecified)
+        }
+    }
 }
