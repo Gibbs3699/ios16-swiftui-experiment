5e042280c6b85c4380c0da54a89f958fe12685ca
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-20 16:21:19 +0700

    add clockHands to RadialLayoutView

--- iOS16/RadialLayoutView.swift
+++ iOS16/RadialLayoutView.swift
@@ -11,6 +11,8 @@ struct RadialLayoutView: View {
     var icons = ["calendar", "message", "figure.walk", "music.note"]
     var numbers = [12,1,2,3,4,5,6,7,8,9,10,11]
     @State var isRadial = true
+    @State var hour: Double = 0
+    @State var minute: Double = 0
     
     var body: some View {
         let layout = isRadial ? AnyLayout(RadialLayout()) : AnyLayout(CustomLayout())
@@ -57,6 +59,12 @@ struct RadialLayoutView: View {
             Circle()
                 .strokeBorder(.black, style: StrokeStyle(lineWidth: 10, dash: [1, 10]))
                 .frame(width: 220)
+            
+            clockHands
+        }
+        .onAppear {
+            hour = 360
+            minute = 360
         }
         .onTapGesture {
             withAnimation(.spring()) {
@@ -89,6 +97,32 @@ struct RadialLayoutView: View {
                     .white.shadow(.inner(color: .gray, radius: 30, x: 30, y: 30))
                 )
                 .frame(width: 280)
+        }
+    }
+    
+    var clockHands: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 4)
+                .foregroundStyle(.black)
+                .frame(width: 8, height: 100)
+                .overlay(RoundedRectangle(cornerRadius: 4).stroke(lineWidth: 1).fill(.white))
+                .offset(y: -46)
+                .rotationEffect(.degrees(minute), anchor: .center)
+                .shadow(radius: 5, y: 5)
+                .animation(.linear(duration: 10).repeatCount(12, autoreverses: false), value: minute)
+            
+            RoundedRectangle(cornerRadius: 4)
+                .foregroundStyle(.black)
+                .frame(width: 8, height: 70)
+                .overlay(RoundedRectangle(cornerRadius: 4).stroke(lineWidth: 1).fill(.white))
+                .offset(y: -32)
+                .rotationEffect(.degrees(hour), anchor: .center)
+                .shadow(radius: 5, y: 5)
+                .animation(.linear(duration: 120), value: hour)
+            
+            Circle()
+                .fill(.white)
+                .frame(width: 3)
         }
     }
 }
