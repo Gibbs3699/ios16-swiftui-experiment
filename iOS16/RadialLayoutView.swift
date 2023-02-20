c0c19267cceeffabaf1dfaf5bd318fae5d259f61
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-20 14:40:53 +0700

    add RadialLayoutView

---
+++ iOS16/RadialLayoutView.swift
@@ -0,0 +1,48 @@
+//
+//  RadialLayoutView.swift
+//  iOS16
+//
+//  Created by TheGIZzz on 20/2/2566 BE.
+//
+
+import SwiftUI
+
+struct RadialLayoutView: View {
+    var icons = ["calendar", "message", "figure.walk", "music.note"]
+    
+    var body: some View {
+        CustomLayout {
+            ForEach(icons, id: \.self) { item in
+                Circle()
+                    .frame(width: 44)
+                            .foregroundColor(.black)
+                    .overlay(Image(systemName: item).foregroundColor(.white))
+            }
+        }
+    }
+}
+
+struct RadialLayoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        RadialLayoutView()
+    }
+}
+
+struct CustomLayout: Layout {
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        proposal.replacingUnspecifiedDimensions()
+    }
+    
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        for (index, subview) in subviews.enumerated() {
+            var point = CGPoint(x: 50*index, y: 50*index).applying(CGAffineTransform(rotationAngle: 5))
+            
+            point.x += bounds.midX
+            point.y += bounds.midY
+            
+            subview.place(at: point, anchor: .center, proposal: .unspecified)
+        }
+    }
+    
+    
+}
