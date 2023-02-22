bb6d146b98bbb6484521af90e63dd01775e81511
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-22 21:33:47 +0700

    add ActionButtonView

---
+++ iOS16/ActionButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  ActionButtonView.swift
+//  iOS16
+//
+//  Created by TheGIZzz on 21/2/2566 BE.
+//
+
+import SwiftUI
+
+struct ActionButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActionButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActionButtonView()
+    }
+}
