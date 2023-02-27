a56d84c1759b8f3e19f10b9f114f106d2ae07b47
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-02-27 18:28:44 +0700

    add more icons and circles

--- iOS16/ActionButtonView.swift
+++ iOS16/ActionButtonView.swift
@@ -26,11 +26,17 @@ struct ActionButtonView: View {
                 .shadow(color: .black.opacity(0.2), radius: 0, x: 1, y: 1)
                 .shadow(color: .black.opacity(0.5), radius: 10, x: 10, y: 10)
                 .overlay(
-                    Image(systemName: "plus")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
+                    icons
+                )
+                .background(
+                    circle.frame(width: 208)
+                        .overlay(circle.frame(width: 60))
+                        .overlay(circle.frame(width: 80))
+                        .offset(x: 60, y: 60)
                         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
-                        .offset(x: -28, y: -28)
+                        .scaleEffect(show ? 1 : 0.8, anchor: .bottomTrailing)
+                        .opacity(show ? 1 : 0)
+                        .animation(.easeOut(duration: 0.3), value: show)
                 )
                 .offset(y: -29)
                 .onTapGesture {
@@ -42,6 +48,38 @@ struct ActionButtonView: View {
         .background(.black)
         .ignoresSafeArea()
         .preferredColorScheme(.dark)
+    }
+    
+    var circle: some View {
+        Circle().stroke(lineWidth: 1).fill(.linearGradient(colors: [.white.opacity(0.5), .clear], startPoint: .topLeading, endPoint: .bottomTrailing))
+    }
+    
+    var icons: some View {
+        ZStack {
+                Image(systemName: "plus")
+                    .font(.system(size: 30))
+                    .foregroundColor(.white)
+                    .rotationEffect(.degrees(show ? 45 : 0), anchor: .center)
+                    .offset(x: -28, y: -28)
+
+                Group {
+                    Image(systemName: "bubble.left.fill")
+                        .foregroundColor(.white)
+                        .offset(x: -28, y: -129)
+
+                    Image(systemName: "moon.fill")
+                        .foregroundColor(.white)
+                        .offset(x: -128, y: -29)
+
+                    Image(systemName: "quote.opening")
+                        .foregroundColor(.white)
+                        .offset(x: -113, y: -114)
+                }
+                .blur(radius: show ? 0 : 10)
+                .opacity(show ? 1 : 0)
+                .scaleEffect(show ? 1 : 0.5)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
     }
     
     var canvas: some View {
